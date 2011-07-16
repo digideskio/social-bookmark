267db2828b9e68b9663726b5c5444abb7ccdf893
rockr <rockr@ubuntu.ubuntu-domain>
2011-07-16 13:19:32 +0530

    created user page

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,10 +1,15 @@
-# Create your views here.
-from django.http import HttpResponse
+# View for the bookmark application
+# Author: @sarat - csarath@gmail.com
+# Date: 15-Jul-2011
+
+from django.http import HttpResponse, Http404
 from django.template import Context
 from django.template.loader import get_template
+from django.contrib.auth.models import User
 
+#generate the main page content
 def main_page(request) :
-	template = get_template('main.html')
+	template = get_template('main_page.html')
 	variables = Context({
 		'head_title': u'Django Bookmarks',
 		'page_title': u'Welcome to Django Bookmarks',
@@ -13,3 +18,22 @@ def main_page(request) :
 	output = template.render(variables)
 	return HttpResponse(output)
 # Context is a object which can take Python Dictionary elements in the constructor)
+
+# generate the user page content
+def user_page(request, username):
+	try:
+		user = User.objects.get(username=username)
+	except User.DoesNotExist:
+		raise Http404(u'Requested user not found');
+
+	bookmarks = user.bookmark_set.all()
+
+
+	template = get_template('user_page.html')
+	variables = Context({ 
+		'username' : username,
+		'bookmarks': bookmarks 
+	})
+	
+	output = template.render(variables)
+	return HttpResponse(output)
