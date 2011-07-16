498c585a1eee20686b5b7b700444a17ab8ea7601
rockr <rockr@ubuntu.ubuntu-domain>
2011-07-16 10:01:13 +0530

    template generation finished

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,16 +1,15 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.template import Context
+from django.template.loader import get_template
+
 def main_page(request) :
-	output = u'''
-		<html>
-			<head><Title>%s</Title></head>
-			<body>  
-				<h1>%s</h1><p>%s</p>
-			</body>
-		</html>
-	''' % ( 
-	u'Django-Bookmarks',
-	u'Weclome to django bookmarks',
-	u'Where you can store and share bookmarks!'
-	)
+	template = get_template('main.html')
+	variables = Context({
+		'head_title': u'Django Bookmarks',
+		'page_title': u'Welcome to Django Bookmarks',
+		'page_body': u'Where you can store and share bookmarks!'
+	})
+	output = template.render(variables)
 	return HttpResponse(output)
+# Context is a object which can take Python Dictionary elements in the constructor)
