67feeddf8c7f5720dcec92a8144a700dfc181276
Sarath C <csarath@gmail.com>
2011-07-22 08:36:54 +0530

    fixed csrf issue

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -6,17 +6,18 @@ from django.http import HttpResponse, Http404
 from django.template import Context
 from django.template.loader import get_template
 from django.contrib.auth.models import User
+from django.shortcuts import render_to_response
 
 #generate the main page content
 def main_page(request) :
-	template = get_template('main_page.html')
-	variables = Context({
+    variables = Context({
 		'head_title': u'Django Bookmarks',
 		'page_title': u'Welcome to Django Bookmarks',
 		'page_body': u'Where you can store and share bookmarks!'
 	})
-	output = template.render(variables)
-	return HttpResponse(output)
+    c.update(csrf(request))
+    return render_to_response( 'main_page.html',variables )
+
 # Context is a object which can take Python Dictionary elements in the constructor)
 
 # generate the user page content
